2212d1cfda87c813f8882a71b1019a87a51305a3
talus005 <expogrow.org@gmail.com>
2019-02-14 22:14:26 +0530

    Bugfix

--- src/main/java/com/training/mjunction/hystrixdashboard/Application.java
+++ src/main/java/com/training/mjunction/hystrixdashboard/Application.java
@@ -2,21 +2,19 @@ package com.training.mjunction.hystrixdashboard;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @Controller
+@EnableAutoConfiguration
 @EnableHystrixDashboard
 @EnableDiscoveryClient
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
 	@RequestMapping("/")
